7af9edef992262cd0e3511d30bfea2603f6e8bcd
Enrico Foschi <foschi.enrico@gmail.com>
2015-05-31 12:28:58 +0200

    Package JS update

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'enricofoschi:crater.js',
-  version: '0.0.6',
+  version: '0.0.7',
   // Brief, one-line summary of the package.
   summary: 'Meteor Micro Framework For Highly Effective Projects',
   // URL to the Git repository containing the source code for this package.
