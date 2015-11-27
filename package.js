7997bd303478eda2fa0f53d1711041a8251daae2
Alessio Scarapazzi <xunga_xunga@hotmail.com>
2015-11-27 17:47:47 +0100

    Small version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'enricofoschi:crater.js',
-  version: '0.0.17',
+  version: '0.0.18',
   // Brief, one-line summary of the package.
   summary: 'Meteor Micro Framework For Highly Effective Projects',
   // URL to the Git repository containing the source code for this package.
